2d4ae27040d0426b8fb7c14290f48d71f28da8ec
itsu020402 <tsubaki394>
2021-09-05 20:16:50 +0900

    ヘッダのリンクを修正

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar class="fixed-top" toggleable="lg" type="dark" variant="dark">
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-navbar-brand class="mx-4" href="#">
+      <b-navbar-brand class="mx-4" to="/" >
         <span class="title-text">マイクラ<span class="sohosai">雙峰祭</span>プロジェクト<span class="year">2021</span></span>
       </b-navbar-brand>
 
       <b-collapse id="nav-collapse" class="navbar-collapse justify-content-end" is-nav>
         <b-navbar-nav>
           <b-nav-item right to="/#about">About</b-nav-item>
-          <b-nav-item right to="/#platforms">プラットフォーム</b-nav-item>
-          <b-nav-item right to="/#join">参加する</b-nav-item>
+          <b-nav-item right to="/#join">参加方法</b-nav-item>
           <b-nav-item roght to="/how_to_play#top">遊び方</b-nav-item>
           <b-nav-item right href="https://twitter.com/micra_sohopjt">Twitter</b-nav-item>
         </b-navbar-nav>
