c1ac52ae117e2f07cf2eee655a0d90db2f470049
itsu-dev <itsu020402@gmail.com>
2021-09-07 16:58:47 +0900

    フッターのデザインを変更

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,14 +1,42 @@
 <template>
   <footer>
-    <b-nav class="bg-dark" variant="dark" align="center">
-      <p class="mt-3 text-white">
-        &copy; 2021 マイクラ雙峰祭プロジェクト, <a href="http://github.com/itsu-dev" class="text-white">Itsu</a><br />
-        <span class="caption">マイクラ雙峰祭プロジェクトはMojangから承認されておらず、Mojangとは関係ありません。</span><br />
-        <br />
-        <b-icon icon="twitter" font-scale="2" class="twitter-icon" onclick="window.location='https://twitter.com/micra_sohopjt'" />
-        <b-icon icon="github" font-scale="2" class="github-icon" onclick="window.location='https://github.com/soho-tsukuba/www.soho.tsukuba.me'" />
-      </p>
-    </b-nav>
+    <b-container fluid>
+      <b-row class="bg-dark pt-4 root" align="left">
+        <div class="col-lg-1"/>
+
+        <div class="col-lg-10">
+          <span class="title-text">マイクラ<span class="sohosai">雙峰祭</span>プロジェクト<span class="year">2021</span></span><br/>
+        </div>
+
+        <div class="col-lg-1"/>
+      </b-row>
+      <b-row class="bg-dark pt-2 pb-4 root" align="left">
+        <div class="col-lg-1"/>
+
+        <div class="col-lg-5">
+          <p>
+            &copy; 2021 マイクラ雙峰祭プロジェクト, <a href="http://github.com/itsu-dev" class="text-white">Itsu</a><br/>
+            <br />
+            <span class="caption">
+              マイクラ雙峰祭プロジェクトはMojangから承認されておらず、Mojangとは関係ありません。
+            </span>
+          </p>
+        </div>
+
+        <div class="col-lg-2" />
+
+        <div class="col-lg-3">
+          <h5 class="title-sub">
+            Links
+          </h5>
+          <a href="https://sohosai.com/">筑波大学 雙峰祭</a><br />
+          <a href="https://twitter.com/micra_sohopjt">Twitter</a><br />
+          <a href="https://github.com/soho-tsukuba/www.soho.tsukuba.me">GitHub</a><br />
+        </div>
+
+        <div class="col-lg-1"/>
+      </b-row>
+    </b-container>
   </footer>
 </template>
 
@@ -19,13 +47,47 @@ export default {
 </script>
 
 <style scoped>
-  .caption {
-    color: lightgray;
-    font-size: 0.8em;
-  }
+.root {
+  color: white;
+}
 
-  .twitter-icon {
-    color: #00ACEE;
-    width: 2.0em;
-  }
+.caption {
+  color: lightgray;
+  font-size: 0.8em;
+}
+
+.twitter-icon {
+  color: #00ACEE;
+  width: 2.0em;
+}
+
+.title-text {
+  font-family: 'DotGothic16', sans-serif;
+  font-size: 1.6em;
+}
+
+.sohosai {
+  color: #6600cc;
+}
+
+.year {
+  color: #00c0d4;
+}
+
+.title-sub {
+  font-family: 'DotGothic16', sans-serif;
+  font-size: 1.2em;
+}
+
+a {
+  color: lightgrey;
+  text-decoration: none;
+  border-bottom-color: lightgrey;
+  border-bottom-width: 1px;
+  border-bottom-style: dashed;
+}
+
+a:hover {
+  color: #00c0d4;
+}
 </style>
